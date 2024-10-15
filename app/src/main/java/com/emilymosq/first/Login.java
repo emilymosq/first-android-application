8d8c5af6ed3e26279d14e5717ef5557f29901fc2
emily <emilymosq@gmail.com>
2024-10-15 11:24:26 +0200

    pantalla de login y comienzo de la pantalla del registro

--- app/src/main/java/com/emilymosq/first/Login.java
+++ app/src/main/java/com/emilymosq/first/Login.java
@@ -1,6 +1,10 @@
 package com.emilymosq.first;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,5 +18,32 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
+
+        Button loginButton = findViewById(R.id.loginButton);
+        TextView loginRegisterText = findViewById(R.id.loginRegisterText);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                launchMain();
+            }
+        });
+            loginRegisterText.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    launchRegister();
+                }
+            });
+        }
+        public void launchMain(){
+            Intent intent = new Intent(Login.this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
+        public void launchRegister(){
+            Intent intent = new Intent(Login.this, Register.class);
+            startActivity(intent);
+        }
 }
