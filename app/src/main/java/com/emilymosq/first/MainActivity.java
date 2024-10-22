03e87859ed8a33adfeb9efd495a4d018e315e214
emily <emilymosq@gmail.com>
2024-10-22 11:09:14 +0200

    prueba

--- app/src/main/java/com/emilymosq/first/MainActivity.java
+++ app/src/main/java/com/emilymosq/first/MainActivity.java
@@ -1,6 +1,10 @@
 package com.emilymosq.first;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView welcomeText = findViewById(R.id.welcomeText);
+        TextView addedText = findViewById(R.id.addedText);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("Usuario", Context.MODE_PRIVATE);
+        String nombre = sharedPreferences.getString("userName", "anonimo");
+        String password = sharedPreferences.getString("userPassword", "contraseña");
+
+        welcomeText.setText("hola " + nombre);
+        addedText.setText("tu contraseña es: " + password);
+
+
+
     }
 }
--- app/src/main/java/com/emilymosq/first/Register.java
+++ app/src/main/java/com/emilymosq/first/Register.java
@@ -1,12 +1,21 @@
 package com.emilymosq.first;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class Register extends AppCompatActivity {
 
@@ -19,6 +28,31 @@ public class Register extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        TextInputLayout registerUsernameTIL = findViewById(R.id.registerUsernameTIL);
+        TextInputLayout registerPasswordTIL = findViewById(R.id.registerPasswordTIL);
+        TextInputLayout registerConfirmPasswordTIL = findViewById(R.id.registerConfirmPasswordTIL);
+        Button registerButton = findViewById(R.id.registerButton);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = String.valueOf(registerUsernameTIL.getEditText().getText());
+                String password = String.valueOf(registerPasswordTIL.getEditText().getText());
+                String userPasswordCheck = String.valueOf(registerConfirmPasswordTIL.getEditText().getText());
+
+                if(!password.equals(userPasswordCheck)){
+                    Toast toast = Toast.makeText(getApplicationContext(), "Tus contraseñas no coinciden",  Toast.LENGTH_SHORT);
+                    toast.show();
+                } else{
+                    SharedPreferences preferences = getSharedPreferences("Usuario", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString("username", username);
+                    editor.putString("password", password);
+                    editor.apply();
+                }
+            }
         });
     }
 }
